0959cfeb9d891a17e488142e376bce5995a2af7f
rohit <rohit.shaw.ku500@gmail.com>
2021-04-08 12:19:41 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,87 @@
+// import React from 'react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
+// import Amplify from "aws-amplify";
+// import awsconfig from './aws-exports';
+// import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+// Amplify.configure(awsconfig);
+
+
+// class App extends React.Component {
+
+// render(){
+//   return (
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+//   );
+
+// }
+
+  
+// }
+
+// function App(){
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+
+// }
+
+// export default withAuthenticator(App,true);
+// export default withAuthenticator(App, {initialAuthState: 'signup'});
+import React from "react";
+import "./App.css";
+import Amplify from "aws-amplify";
+import {AmplifyAuthenticator, AmplifyGreetings} from "@aws-amplify/ui-react";
+import {AuthState, onAuthUIStateChange} from "@aws-amplify/ui-components";
+import awsconfig from "./aws-exports";
+
+Amplify.configure(awsconfig);
+
+const GreetingsApp = () => {
+  const [authState, setAuthState] = React.useState();
+  const [user, setUser] = React.useState();
+
+  React.useEffect(() => {
+    return onAuthUIStateChange((nextAuthState, authData) => {
+      setAuthState(nextAuthState);
+      setUser(authData);
+    });
+  }, []);
+
+  return authState === AuthState.SignedIn && user ? (
     <div className="App">
+      <AmplifyGreetings username={user.username}></AmplifyGreetings>
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
@@ -19,7 +97,11 @@ function App() {
         </a>
       </header>
     </div>
+      
+    </div>
+  ) : (
+    <AmplifyAuthenticator />
   );
-}
+};
 
-export default App;
+export default GreetingsApp;
